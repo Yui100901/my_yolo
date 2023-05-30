6725fd68e0c97997916d43ce82f74600dada8e9a
Yui100901 <13757191910yfy@gmail.com>
2023-05-30 20:22:03 +0800

    配置完成

--- my_train.py
+++ my_train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('datasets/yolov8.yaml')
 
-model.train(data='yolo-dc.yaml', workers=2, epochs=50, batch=16)
+model.train(data='datasets/data.yaml', workers=2, epochs=50, batch=16)
